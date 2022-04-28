e90ac8a78f04ae350dc5042c4563d7bb37014a89
Phillip Mwaniki <codezeroprivate@gmail.com>
2022-04-29 01:07:29 +0300

    added cart table

--- app/Models/Variation.php
+++ app/Models/Variation.php
@@ -7,12 +7,17 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Spatie\Image\Manipulations;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 use Staudenmeir\LaravelAdjacencyList\Eloquent\HasRecursiveRelationships;
 
-class Variation extends Model
+class Variation extends Model implements HasMedia
 {
     use HasFactory;
     use HasRecursiveRelationships;
+    use InteractsWithMedia;
 
     public function formattedPrice(): Money
     {
@@ -47,5 +52,20 @@ class Variation extends Model
     public function stocks(): HasMany
     {
         return $this->hasMany(Stock::class);
+    }
+
+    /**
+     * @throws \Spatie\Image\Exceptions\InvalidManipulation
+     */
+    public function registerMediaConversions(?Media $media = null): void
+    {
+        $this->addMediaConversion('thumb200x200')
+             ->fit(Manipulations::FIT_CROP, 200, 200);
+    }
+
+    public function registerMediaCollections(): void
+    {
+        $this->addMediaCollection('default')
+             ->useFallbackUrl(url('/storage/no-product-image.jpg'));
     }
 }
